d171e088935b7aabba4566c00dde0137588398ab
Sunghee Kim <50495214+xxunghee@users.noreply.github.com>
2020-09-26 00:32:17 +0900

    bug fix

--- Arduino/oP/oP.ino
+++ Arduino/oP/oP.ino
@@ -4,17 +4,16 @@
 #include <LiquidCrystal_I2C.h>    // LCD 1602 I2C용 라이브러리
 #include <Adafruit_NeoPixel.h>                // RGB_LED 사용을 위한 라이브러리 선언
 
-#define Tx 7        // 블루투스 송신
-#define Rx 8        // 블루투스 수신
-#define led_foot 6  // 원형 LED
+#define Tx 7          // 블루투스 송신
+#define Rx 8          // 블루투스 수신
+#define led_foot 6    // 원형 LED
 #define led_R A3      // 좌석 사용 알림 LED
 #define led_G A4      // 좌석 사용 알림 LED
 #define led_B A5      // 좌석 사용 알림 LED
 
 SoftwareSerial bluetooth(Tx, Rx);     // 블루투스 통신
 LiquidCrystal_I2C lcd(0x27, 16, 2);   // LCD 접근 주소 : 0x3F or 0x27
-Adafruit_NeoPixel circle = Adafruit_NeoPixel(3, led_foot, NEO_GRB); // 3개의 LED와 제어핀을 6번 핀으로 설정.
-
+Adafruit_NeoPixel circle = Adafruit_NeoPixel(12, led_foot, NEO_GRB); // 8개의 LED와 제어핀 설정 
 
 void setup() {
   Serial.begin(9600);
@@ -33,26 +32,25 @@ void setup() {
 }
 
 void loop() {
-  BluetoothControl();
-}
+    if (bluetooth.available()) {
+    char value = bluetooth.read();
 
-void BluetoothControl() {
-  if (bluetooth.available()) {
+    Serial.println(value);
 
     // on 버튼 눌렀을 때
-    if () {
-      LEDControl(255, 0, 0);
+    if (value == '1') {
+      LEDControl(0, 255, 0);
       LCDControl(1);   // 자리 비워주세요 < 표시
     }
 
-    //off 버튼 눌렀을 때
-    else if () {
+    // off 버튼 눌렀을 때
+    else if (value == '0') {
       LEDControl(0, 0, 0);
       LCDControl(0);   // 사용중 < 표시
     }
 
     delay(180000);
-  }
+  } 
 }
 
 void LEDControl(int r, int g, int b) {
@@ -61,12 +59,23 @@ void LEDControl(int r, int g, int b) {
   analogWrite(led_G, g);
   analogWrite(led_B, b);
 
+  colorWipe(circle.Color(255, 0, 0), 50);
+
   // 발 밑의 LED 제어
-  for (int i = 0; i < 8; i++) {
-    circle.setPixelColor(i, 255, 0, 0);
-    circle.show();
+  //for (int i = 0; i < 12; i++) {
+  //  circle.setPixelColor(i, 255, 0, 0);
+  //  circle.show();
+  //}
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<circle.numPixels(); i++) {
+      circle.setPixelColor(i, c);
+      circle.show();
+      delay(wait);
   }
 }
+
 
 void LCDControl(int i) {
   if (i == 1) // on
