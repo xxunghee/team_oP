a7db0e7112044938a1aa41815f5f7d03f4e5482b
Sunghee Kim <50495214+xxunghee@users.noreply.github.com>
2020-09-26 00:52:41 +0900

    v1.0.0

--- Arduino/oP/oP.ino
+++ Arduino/oP/oP.ino
@@ -2,7 +2,7 @@
 #include <SoftwareSerial.h>       // 블루투스 통신을 위한 라이브러
 #include <Wire.h>                 // I2C 통신을 위한 라이브러리 
 #include <LiquidCrystal_I2C.h>    // LCD 1602 I2C용 라이브러리
-#include <Adafruit_NeoPixel.h>                // RGB_LED 사용을 위한 라이브러리 선언
+#include <Adafruit_NeoPixel.h>    // RGB_LED 사용을 위한 라이브러리 선언
 
 #define Tx 7          // 블루투스 송신
 #define Rx 8          // 블루투스 수신
@@ -13,7 +13,7 @@
 
 SoftwareSerial bluetooth(Rx, Tx);     // 블루투스 통신
 LiquidCrystal_I2C lcd(0x27, 16, 2);   // LCD 접근 주소 : 0x3F or 0x27
-Adafruit_NeoPixel circle = Adafruit_NeoPixel(12, led_foot, NEO_GRB); // 8개의 LED와 제어핀 설정 
+Adafruit_NeoPixel circle = Adafruit_NeoPixel(12, led_foot, NEO_GRB); // 8개의 LED와 제어핀 설정
 
 void setup() {
   Serial.begin(9600);
@@ -32,7 +32,7 @@ void setup() {
 }
 
 void loop() {
-    if (bluetooth.available()) {
+  if (bluetooth.available()) {
     char value = bluetooth.read();
 
     Serial.println(value);
@@ -48,11 +48,10 @@ void loop() {
     else if (value == '0') {
       LEDControl(0, 0, 0);
       LCDControl(0);   // 사용중 < 표시
-      colorWipe(circle.Color(255, 0, 0), 500);
+      circle.fill((0, 0, 0));
+      circle.show();
     }
-
-    delay(180000);
-  } 
+  }
 }
 
 void LEDControl(int r, int g, int b) {
@@ -63,15 +62,16 @@ void LEDControl(int r, int g, int b) {
 }
 
 void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<circle.numPixels(); i++) {
-      circle.setPixelColor(i, c);
-      circle.show();
-      delay(wait);
+  for (uint16_t i = 0; i < circle.numPixels(); i++) {
+    circle.setPixelColor(i, c);
+    circle.show();
+    delay(wait);
   }
 }
 
 
 void LCDControl(int i) {
+  // on
   if (i == 1) // on
   {
     lcd.setCursor(0, 0);
@@ -80,7 +80,8 @@ void LCDControl(int i) {
     lcd.print("for pregnant woman.");
   }
 
-  else { // off
+  // off
+  else {
     lcd.init();
   }
 }
